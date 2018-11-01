62e3115cf57797340b0fd8dce91018573ec644b9
David Rose <doppler@gmail.com>
2018-11-01 15:01:30 -0400

    More access-control stuff

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ app.get("/", (req, res) => {
 
 app.get("/:locationId", async (req, res) => {
   res.set("Access-Control-Allow-Origin", "*");
+  res.set("Access-Control-Allow-Credentials", "false");
+  res.set("Access-Control-Allow-Headers", "Content-Type");
   res.set("Access-Control-Allow-Methods", "GET,OPTIONS");
   const sequelize = await configureSequelize(req.params.locationId);
   const data = await fetchData(sequelize);
